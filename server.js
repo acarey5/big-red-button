06ab4c5426a120dadce52730682caeeb9bac62d8
acarey5 <acarey5@uncc.edu>
2025-05-28 21:34:55 -0400

    fix

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+  console.log(`Server running at http://localhost:${PORT}`);
 });
